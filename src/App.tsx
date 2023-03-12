9ca51d2a43ef44b0c04f6824a2a1f7029b23fc0a
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-03-12 21:45:27 +0700

    feat: added basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,37 @@ function App() {
     setEmail(e.target.value)
   }
 
+  const handleEnterDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleAuth()
+    }
+  }
+
+  const handleAuth = () => {
+    fetch("http://localhost:4000/emails")
+      .then((res) => res.json())
+      .then((emailsArray) => {
+        const currentObject = emailsArray.find(
+          (emailObj: any) => emailObj[email]
+        )
+        if (currentObject) {
+          console.log("currentData", currentObject)
+        } else {
+          const params = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ [email]: email }),
+          }
+          fetch("http://localhost:4000/add", params)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+        }
+        setEmail("")
+      })
+  }
+
   useEffect(() => {
     console.log("email", email)
   }, [email])
@@ -18,12 +49,14 @@ function App() {
         <div className="input_label">Enter email</div>
         <div className="input_form">
           <input
+            onKeyDown={(e) => handleEnterDown(e)}
+            value={email}
             type="text"
             className="input_form_field"
             onChange={handleEmailChange}
           />
           <div className="input_form_btn">
-            <div>Go</div>
+            <div onClick={handleAuth}>Go</div>
           </div>
         </div>
       </div>
