7531aa68dc80ecff35540d8bb474147983934813
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-03-12 22:18:54 +0700

    feat:info

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import "./App.scss"
 
 function App() {
   const [email, setEmail] = useState("")
+  const [wallet, setWallet] = useState<any>()
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
@@ -15,33 +17,49 @@ function App() {
   }
 
   const handleAuth = () => {
-    fetch("http://localhost:4000/emails")
-      .then((res) => res.json())
-      .then((emailsArray) => {
-        const currentObject = emailsArray.find(
-          (emailObj: any) => emailObj[email]
-        )
-        if (currentObject) {
-          console.log("currentData", currentObject)
-        } else {
-          const params = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ [email]: email }),
+    if (email) {
+      fetch("http://localhost:4000/emails")
+        .then((res) => res.json())
+        .then((emailsArray) => {
+          const existingWallet = emailsArray.find(
+            (emailObj: any) => emailObj[email]
+          )
+          if (existingWallet) {
+            console.log("existingWallet", existingWallet)
+            setWallet(existingWallet[email])
+          } else {
+            const newWallet = ethers.Wallet.createRandom()
+            console.log("newWallet", newWallet)
+            const walletData = {
+              address: newWallet.address,
+              mnemonic: newWallet?.mnemonic?.phrase,
+            }
+
+            setWallet(walletData)
+
+            const params = {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ [email]: walletData }),
+            }
+            fetch("http://localhost:4000/add", params)
+              .then((res) => res.json())
+              .then((res) => console.log(res))
           }
-          fetch("http://localhost:4000/add", params)
-            .then((res) => res.json())
-            .then((res) => console.log(res))
-        }
-        setEmail("")
-      })
+          setEmail("")
+        })
+    }
   }
 
   useEffect(() => {
     console.log("email", email)
   }, [email])
+
+  useEffect(() => {
+    console.log("wallet", wallet)
+  }, [wallet])
 
   return (
     <div className="app">
@@ -60,6 +78,14 @@ function App() {
           </div>
         </div>
       </div>
+      {wallet && (
+        <>
+          <div className="transaction">
+            <div className="transaction_btn">Send ETH</div>
+          </div>
+          <div className="address">{wallet.address}</div>
+        </>
+      )}
     </div>
   )
 }
