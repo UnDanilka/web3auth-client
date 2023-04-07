beca5d748d434ab7a2ae7ad635e700154fadfcf0
Danil Elfimov <dancingdanil98@gmail.com>
2023-04-07 16:07:07 +0700

    lil refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import "./App.scss"
+
 import {
-  types,
-  deadline,
-  transferLimit,
-  vitacoreWallet,
-  vft,
-} from "./utils/constants"
-import {
+  burn,
+  checkAllowance,
+  checkBalance,
   createWallet,
-  getDomain,
-  getGasPrice,
+  mint,
   transferTokens,
+  signPermission,
 } from "./utils/utils"
 
 function App() {
@@ -72,99 +69,22 @@ function App() {
     }
   }
 
-  const getSignature = async () => {
-    const nonces = await vft.nonces(userWallet.address)
-
-    const values = {
-      owner: userWallet.address,
-      spender: vitacoreWallet.address,
-      value: transferLimit,
-      nonce: nonces,
-      deadline: deadline,
-    }
-
-    const domain = await getDomain()
-
-    const signature = await userWallet._signTypedData(domain, types, values)
-
-    const sig = ethers.utils.splitSignature(signature)
-
-    return sig
-  }
-
-  const signPermission = async () => {
-    console.log("signing permission started")
-
-    const sig = await getSignature()
-
-    let permitHash = await vft.permit(
-      userWallet.address,
-      vitacoreWallet.address,
-      transferLimit,
-      deadline,
-      sig.v,
-      sig.r,
-      sig.s,
-      {
-        gasPrice: await getGasPrice(),
-        gasLimit: 80000,
-      }
-    )
-
-    await permitHash.wait(2)
-    console.log("signing permission finished")
-  }
-
-  const checkAllowance = async () => {
-    const tokenAllowance = await vft.allowance(
-      userWallet.address,
-      vitacoreWallet.address
-    )
-
-    console.log(`Check allowance of tokenReceiver: ${tokenAllowance}`)
-    return tokenAllowance
-  }
-
-  const checkBalance = async () => {
-    const vitacoreWalletBalance = await vft.balanceOf(vitacoreWallet.address)
-    const userWalletBalance = await vft.balanceOf(userWallet.address)
-
-    console.log(`vitacoreWallet balance: ${vitacoreWalletBalance / 10 ** 18}`)
-    console.log(`userWallet balance: ${userWalletBalance / 10 ** 18}`)
-  }
-
-  const burn = async () => {
-    const hash = await vft.burn(ethers.utils.parseEther("10"))
-    console.log(hash)
-    hash.wait()
-  }
-
-  const mint = async () => {
-    console.log("minting started")
-    const hash = await vft.mint(
-      userWallet.address,
-      ethers.utils.parseEther("100")
-    )
-    hash.wait()
-    console.log("minting finished")
-  }
-
   useEffect(() => {
     console.log("email", email)
   }, [email])
 
-  useEffect(() => {
-    if (userWallet) {
-      const permissionCondition = async () => {
-        const tokenAllowance = await checkAllowance()
-        if (+tokenAllowance === 0) {
-          await signPermission()
-        }
-      }
-      permissionCondition()
-    }
-    console.log("userWallet", userWallet)
-  }, [userWallet])
+  // useEffect(() => {
+  //   if (userWallet) {
+  //     const permissionCondition = async () => {
+  //       const tokenAllowance = await checkAllowance()
+  //       if (+tokenAllowance === 0) {
+  //         await signPermission()
+  //       }
+  //     }
+  //     permissionCondition()
+  //   }
+  //   console.log("userWallet", userWallet)
+  // }, [userWallet])
 
   return (
     <div className="app">
@@ -183,19 +103,19 @@ function App() {
           </div>
         </div>
         <div className="test">
-          <div className="test_btn" onClick={mint}>
+          <div className="test_btn" onClick={() => mint(userWallet)}>
             Mint
           </div>
-          <div className="test_btn" onClick={checkBalance}>
+          <div className="test_btn" onClick={() => checkBalance(userWallet)}>
             Check balance
           </div>
           <div className="test_btn" onClick={burn}>
             Burn
           </div>
-          <div className="test_btn" onClick={checkAllowance}>
+          <div className="test_btn" onClick={() => checkAllowance(userWallet)}>
             Check allowance
           </div>
-          <div className="test_btn" onClick={signPermission}>
+          <div className="test_btn" onClick={() => signPermission(userWallet)}>
             Sign permission
           </div>
         </div>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,12 @@
 import { ethers } from "ethers"
-import { vitacoreWallet, vft, provider } from "./constants"
+import {
+  vitacoreWallet,
+  vft,
+  provider,
+  types,
+  deadline,
+  transferLimit,
+} from "./constants"
 
 export const createWallet = (mnemonic: string) => {
   const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic)
@@ -10,6 +17,7 @@ export const createWallet = (mnemonic: string) => {
 
 export const transferTokens = async (amount: string, userWallet: any) => {
   console.log("transfer started")
+
   const transferFromHash = await vft.transferFrom(
     userWallet.address,
     vitacoreWallet.address,
@@ -43,4 +51,81 @@ export const getDomain = async () => {
 export const getGasPrice = async () => {
   const gasPrice = await provider.getGasPrice()
   return gasPrice
+}
+
+const getSignature = async (userWallet: any) => {
+  const nonces = await vft.nonces(userWallet.address)
+
+  const values = {
+    owner: userWallet.address,
+    spender: vitacoreWallet.address,
+    value: transferLimit,
+    nonce: nonces,
+    deadline: deadline,
+  }
+
+  const domain = await getDomain()
+
+  const signature = await userWallet._signTypedData(domain, types, values)
+
+  const sig = ethers.utils.splitSignature(signature)
+
+  return sig
+}
+
+export const signPermission = async (userWallet: any) => {
+  console.log("signing permission started")
+
+  const sig = await getSignature(userWallet)
+
+  let permitHash = await vft.permit(
+    userWallet.address,
+    vitacoreWallet.address,
+    transferLimit,
+    deadline,
+    sig.v,
+    sig.r,
+    sig.s,
+    {
+      gasPrice: await getGasPrice(),
+      gasLimit: 80000,
+    }
+  )
+
+  await permitHash.wait(2)
+  console.log("signing permission finished")
+}
+
+export const checkAllowance = async (userWallet: any) => {
+  const tokenAllowance = await vft.allowance(
+    userWallet.address,
+    vitacoreWallet.address
+  )
+
+  console.log(`Check allowance of tokenReceiver: ${tokenAllowance}`)
+  return tokenAllowance
+}
+
+export const checkBalance = async (userWallet: any) => {
+  const vitacoreWalletBalance = await vft.balanceOf(vitacoreWallet.address)
+  const userWalletBalance = await vft.balanceOf(userWallet.address)
+
+  console.log(`vitacoreWallet balance: ${vitacoreWalletBalance / 10 ** 18}`)
+  console.log(`userWallet balance: ${userWalletBalance / 10 ** 18}`)
+}
+
+export const burn = async () => {
+  const hash = await vft.burn(ethers.utils.parseEther("10"))
+  console.log(hash)
+  hash.wait()
+}
+
+export const mint = async (userWallet: any) => {
+  console.log("minting started")
+  const hash = await vft.mint(
+    userWallet.address,
+    ethers.utils.parseEther("100")
+  )
+  hash.wait()
+  console.log("minting finished")
 }
